7006fb0bdee7c6fa4966e9a882fe69c40903d17a
J K <jkim65@buffalo.edu>
2020-10-26 03:10:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,11 +4,11 @@ class PigLatinizer
     ay = "ay"
     way = "way"
     arr = input.split.map do |word|
-      if word[0].match(/[aeiouAEIOU]/)
+      if word[0].match(/[aeiou]/)
         new_word = word + way
         new_word
       else
-        consonants = word.slice!(/^([aeiou]+)/)
+        consonants = word.slice!(/^([^aeiou]+)/)
         new_word = word + consonants + ay
         new_word
       end
